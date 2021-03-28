ba9d1ff2bb96c5a162c96a570c1e7326eecbefdd
Ruzeen Rezvie <i@ruzeen.com>
2021-03-27 21:13:34 -0700

    Create investingCoffeeMoney.py

---
+++ investingCoffeeMoney.py
@@ -0,0 +1,23 @@
+def investingCoffeeMoney(coffee_price, times_per_week, num_years, cagr):
+  cagr_val = (100+cagr)/100
+  yearly_spend = coffee_price*times_per_week*52
+  start = yearly_spend*cagr_val
+
+  for i in range(1,num_years+1):
+    if i > 1:
+      next = (start + yearly_spend)*cagr_val
+      start = next
+      if i == num_years:
+        savings = round(next,2)
+        print('You will have',savings,'if you invest your coffee $ for %s years.'%i)
+
+# How much do you pay for a cup of coffee?
+coffee_price = 2.50
+# How many times per week do you buy coffee?
+times_per_week = 5
+# How many years can you go without buying coffee? :D
+num_years = 5
+# Compound Annual Growth Rate (ROI %)
+cagr = 15
+
+investingCoffeeMoney(coffee_price, times_per_week, num_years, cagr)
