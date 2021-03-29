3dbf30166e2a104fa355cb4744ad830402f3c931
Ruzeen Rezvie <i@ruzeen.com>
2021-03-28 20:39:37 -0700

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -3,18 +3,17 @@
 If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1. '''
 
 def collatz(n):
-  steps = 0
-  if n > 1:
+  if n < 2:
+    return 'Please input a number greater than 1.'
+  else:
+    steps = 0
     while n > 1:
       if n%2 == 0:
         n/=2
         steps += 1
       else:
         n = (n*3) + 1
-        steps += 1
-        if n%2 == 0:
-          n/=2
+        n/=2
+        for i in range(2):
           steps += 1
     return steps
-  else:
-    return 'Please input a number greater than 1.'
